e89004a67a9559b99acb9ad90680853b7791b662
Faresjj <jelassi.fares@gmail.com>
2020-06-01 16:42:37 +0100

    new commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import Login from './Components/Login';
+import './App.css';
+
+class App extends Component {
+    render() {  
+        return (
+            <div>
+                <Router>
+                    <nav className="navbar"> 
+                        <ul className="navbar-list">
+                            <li className="navbar-item">
+                                <Link to="/mynetwork" className="navbar-link">My Network</Link>
+                            </li>
+                            <li className="navbar-item">
+                                <Link to="/messaging" className="navbar-link">Messaging</Link>
+                            </li>
+                        </ul>
+
+                    </nav>
+
+                     <Route path="/" exact component={Login}/> 
+
+                </Router>
+            </div>
+        );
+    }
+}
+export default App;
